4bb32ecd773eeba8fd534aacaa2055d29a778be3
Jiří Špác <capajj@gmail.com>
2013-07-18 22:54:48 +0200

    voting WIP2

--- voting.js
+++ voting.js
@@ -14,19 +14,38 @@ module.exports = function(url, callback) {
                     var protocolLink =  h1.children()[0];
                     var meetingNum = Number(h1.text().split('.')[0]);
                     var voteNum =  Number(protocolLink.innerHTML.split('.')[0]);
+                    var voteItems = main.find('li');
+                    if (voteItems.length === 200) {
+                        var allVotes = [];
+                        for (var i = 0; i < voteItems.length; i++) {
+                            var vote = voteItems[i];
+                            var span = vote.firstElementChild;
+                            var attr = span.attributes[0].value;
+                            var voteVal = attr.split(' ')[1];
+                            var atag = vote.children[1];
+                            var name = atag.innerText;
+                            allVotes.push({name: name, val: voteVal});
+                        }
+                        var ret = {
+                            votes: allVotes,
+                            h1: h1,
+                            meeting: meetingNum,
+                            voteNumber: voteNum
+                        };
+                        return JSON.stringify(ret);
+
+
+                    } else {
+                        console.warn("scrape voting for " + url + " failed. We counted " + voteItems.length + " votes.");
+                    }
                 } else {
                     return false;
                 }
             }, function (scraped) {
                 if (scraped) {
-                    scraped = JSON.parse(scraped);
-
-
-                    var voting = {number: printNumber, name: printName, state: scraped.state};
-                    //accepted, refused, ongoing
-                    callback(voting) ;
+                    callback(JSON.parse(scraped));
                 } else {
-                    console.warn("scrape printHistory for " + url + " failed.");
+                    console.warn("scrape voting for " + url + " failed.");
                 }
             });
         });
